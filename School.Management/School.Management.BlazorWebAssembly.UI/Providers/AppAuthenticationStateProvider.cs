9df260e461aa76a0e18a9dbdb79e986685be2912
William Amaya <william.amaya@sykes.com>
2024-06-04 16:59:23 -0600

    se ha realizado autenticacion falsa para realizar pruebas

--- School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
+++ School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
@@ -7,10 +7,20 @@ namespace School.Management.BlazorWebAssembly.UI.Providers
     {
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+        }
+
+        public void Authenticate()
+        {
             List<Claim> claims = [new Claim(ClaimTypes.Name, "William"), new Claim(ClaimTypes.Role, "Administrator")];
-            ClaimsIdentity identity = new (claims, "Custom");
+            ClaimsIdentity identity = new(claims, "Custom");
             ClaimsPrincipal principal = new(identity);
-            return Task.FromResult(new AuthenticationState(principal));
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(principal)));
+        }
+
+        public void Logout()
+        {
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new ClaimsPrincipal())));
         }
     }
 }
