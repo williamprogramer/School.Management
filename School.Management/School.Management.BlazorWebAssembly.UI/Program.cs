07817f629e6db861c12855e1137ce6bbf478b7a1
William Amaya <william.amaya@sykes.com>
2024-06-07 22:54:11 -0600

    # se esta avanzando en mejorar el flujo de autenticacion

--- School.Management/School.Management.BlazorWebAssembly.UI/Program.cs
+++ School.Management/School.Management.BlazorWebAssembly.UI/Program.cs
@@ -1,15 +1,16 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using School.Management.BlazorWebAssembly.UI;
 using MudBlazor.Services;
+using School.Management.BlazorWebAssembly.UI;
 using School.Management.BlazorWebAssembly.UI.Providers;
-using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7265/") });
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7265/") });
 builder.Services.AddMudServices();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthorizationCore();
--- School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
+++ School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
@@ -3,18 +3,44 @@ using System.Security.Claims;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Blazored.LocalStorage;
 
 namespace School.Management.BlazorWebAssembly.UI.Providers
 {
-    public class AppAuthenticationStateProvider : AuthenticationStateProvider
+    public class AppAuthenticationStateProvider(ILocalStorageService localStorage) : AuthenticationStateProvider
     {
-        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+            bool hasToken = await localStorage.ContainKeyAsync("token");
+            if (!hasToken)
+            {
+                return new AuthenticationState(new ClaimsPrincipal());
+            }
+            else
+            {
+                string? token = await localStorage.GetItemAsync<string>("token");
+                SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes("gEWsrPC7h5oxD9LkniwHOMd4TeN2acB6"));
+                TokenValidationParameters validationParameters = new()
+                {
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = securityKey,
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://localhost:7265",
+                    ValidateAudience = true,
+                    ValidAudience = "http://localhost:5263"
+                };
+                JsonWebTokenHandler tokenHandler = new();
+                TokenValidationResult tokenValidation = await tokenHandler.ValidateTokenAsync(token, validationParameters);
+                ClaimsIdentity identity = new(tokenValidation.ClaimsIdentity.Claims, "JWT");
+                ClaimsPrincipal principal = new(identity);
+                return new AuthenticationState(principal);
+            }
         }
 
         public async Task AuthenticateAsync(string token)
         {
+            await localStorage.SetItemAsync("token", token);
             SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes("gEWsrPC7h5oxD9LkniwHOMd4TeN2acB6"));
             TokenValidationParameters validationParameters = new()
             {
@@ -28,16 +54,15 @@ namespace School.Management.BlazorWebAssembly.UI.Providers
             };
             JsonWebTokenHandler tokenHandler = new();
             TokenValidationResult tokenValidation = await tokenHandler.ValidateTokenAsync(token, validationParameters);
-
-            List<Claim> claims = [new Claim(ClaimTypes.Name, "William"), new Claim(ClaimTypes.Role, "Administrator")];
-            ClaimsIdentity identity = new(claims, "Custom");
+            ClaimsIdentity identity = new(tokenValidation.ClaimsIdentity.Claims, "JWT");
             ClaimsPrincipal principal = new(identity);
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(principal)));
         }
 
-        public void Logout()
+        public async Task Logout()
         {
             NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new ClaimsPrincipal())));
+            await localStorage.RemoveItemAsync("token");
         }
     }
 }
