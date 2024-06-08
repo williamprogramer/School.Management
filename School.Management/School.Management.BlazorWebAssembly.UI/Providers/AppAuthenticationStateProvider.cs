15091cc667b107ff99a19fbac8010bfdd798b51c
William Amaya <william.amaya@sykes.com>
2024-06-07 19:35:52 -0600

    # se esta trabajando en agregar la creacion y validacion de token en el front end la creacion la hace la api

--- School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
+++ School.Management/School.Management.BlazorWebAssembly.UI/Providers/AppAuthenticationStateProvider.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace School.Management.BlazorWebAssembly.UI.Providers
 {
@@ -10,8 +13,22 @@ namespace School.Management.BlazorWebAssembly.UI.Providers
             return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
         }
 
-        public void Authenticate()
+        public async Task AuthenticateAsync(string token)
         {
+            SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes("gEWsrPC7h5oxD9LkniwHOMd4TeN2acB6"));
+            TokenValidationParameters validationParameters = new()
+            {
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+                ValidateIssuer = true,
+                ValidIssuer = "https://localhost:7265",
+                ValidateAudience = true,
+                ValidAudience = "http://localhost:5263"
+            };
+            JsonWebTokenHandler tokenHandler = new();
+            TokenValidationResult tokenValidation = await tokenHandler.ValidateTokenAsync(token, validationParameters);
+
             List<Claim> claims = [new Claim(ClaimTypes.Name, "William"), new Claim(ClaimTypes.Role, "Administrator")];
             ClaimsIdentity identity = new(claims, "Custom");
             ClaimsPrincipal principal = new(identity);
--- School.Management/School.Management.Rest.API/Controllers/AuthController.cs
+++ School.Management/School.Management.Rest.API/Controllers/AuthController.cs
@@ -1,15 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace School.Management.Rest.API.Controllers
 {
+    public class AuthRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
     [ApiController]
     [Route("api/[controller]")]
+    [AllowAnonymous]
     public class AuthController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        [HttpPost]
+        public IActionResult Post([FromBody] AuthRequest request)
         {
-            return Ok("success");
+            if (request.Name == "amayawa" && request.Password == "gEWsrPC7h5oxD9LkniwHOMd4TeN2acB6")
+            {
+                SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes("gEWsrPC7h5oxD9LkniwHOMd4TeN2acB6"));
+                SigningCredentials credentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+                List<Claim> claims =
+                [
+                    new(JwtRegisteredClaimNames.Sub, Guid.NewGuid().ToString()),
+                    new(JwtRegisteredClaimNames.Name, "William Amaya"),
+                    new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new(ClaimTypes.Role, "Administrator"),
+                    new(ClaimTypes.Name, "William Amaya")
+                ];
+
+                SecurityTokenDescriptor tokenDescriptor = new()
+                {
+                    Issuer = "https://localhost:7265",
+                    Audience = "http://localhost:5263",
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.UtcNow.AddHours(8),
+                    SigningCredentials = credentials
+                };
+
+                JsonWebTokenHandler tokenHandler = new();
+                string token = tokenHandler.CreateToken(tokenDescriptor);
+                return Ok(new { Token = token });
+            }
+
+            return NotFound();
         }
     }
 }
