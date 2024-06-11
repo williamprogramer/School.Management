d7e14a8b6cf0bbdf7844d230b33edf6292b1ece8
William Amaya <william.amaya@sykes.com>
2024-06-11 13:12:20 -0600

    # se esta trabajando en el desing de form para agregar administradores

--- School.Management/School.Management.Rest.API/Program.cs
+++ School.Management/School.Management.Rest.API/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddMediatR(options => options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 builder.Services.AddCors(options => {
-    options.AddPolicy("AllowCors", builder =>
+    options.AddPolicy("AppCors", builder =>
     {
         builder.WithOrigins("https://localhost:44300")
             .AllowAnyHeader()
@@ -14,7 +14,7 @@ builder.Services.AddCors(options => {
 
 var app = builder.Build();
 app.UseHttpsRedirection();
-app.UseCors("AllowCors");
+app.UseCors("AppCors");
 app.MapControllers();
 
 app.Run();
